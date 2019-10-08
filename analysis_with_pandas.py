ca23347aa82a9e4dba2e2adb38e2f6ded8563981
Barrantes Quesada, Mario Alberto <mario.alberto.barrantes.quesada@intel.com>
2019-10-08 07:14:29 -0600

    Fixing some issues with the data

--- analysis_with_pandas.py
+++ analysis_with_pandas.py
@@ -1,20 +1,72 @@
 import pandas as pd
 
-##############################################
-#MFT: Matrícula Total.
-#MFH: Matrícula Total Hombres
-#MFM: Matrícula Total Mujeres
-#RET: Total Reprobados
-#REH: Total Hombres Reprobados
-#REM: Total Mujeres Reprobadas
-#APT: Total Aprobados
-#APH: Total Hombres Aprobados
-#APM: Total Mujeres Aprobadas
+"""
+Nomenclatura Fuente:
+    MFT: Matrícula Total.
+    MFH: Matrícula Total Hombres
+    MFM: Matrícula Total Mujeres
+    RET: Total Reprobados
+    REH: Total Hombres Reprobados
+    REM: Total Mujeres Reprobadas
+    APT: Total Aprobados
+    APH: Total Hombres Aprobados
+    APM: Total Mujeres Aprobadas
+    
+    TOTT: Total Extranjeros
+    TOTH: Total Hombres Extranjeros
+    TOTM: Total Mujeres Extranjeros
+    
+Nomenclatura datos procesados:
+    ETM = Escuelas Total Matricualados
+    EHM = Escuelas Hombres Matricualados
+    EMM = Escuelas Mujeres Matricualados
+    ETR = Escuelas Total Reprobados
+    EHR = Escuelas Hombres Reprobados
+    EMR = Escuelas Mujeres Reprobadas
+    ETA = Escuelas Total Aprobados
+    EHA = Escuelas Hombres Aprobados
+    EMA = Escuelas Mujeres Aprobadas
+    EPU = Escuela Publica
+    EPR = Escuela Privada
+    ESV = Escuela Subvencionada
+    EUR = Escuela Urbana
+    ERU = Escuela Rural 
+    ETAA = Escuela Tasa de Aprobacion
+    ETAAH = Escuela Tasa de Aprobacion Hombres
+    ETAAM = Escuela Tasa de Aprobacion Mujeres
+    EEXM = Escuela Extranjeros Matriculads
+    EPEXM = Escuela Proporcion Extranjeros Matriculads
+    ET = Escuelas en Total
+    EPPR = Escuelas Proporcion de Privadas
+    EPRU = Escuelas Proporcion de Rurales
+    
+    CTM = Colegios Total Matricualados
+    CHM = Colegios Hombres Matricualados
+    CMM = Colegios Mujeres Matricualados
+    CTR = Colegios Total Reprobados
+    CHR = Colegios Hombres Reprobados
+    CMR = Colegios Mujeres Reprobadas
+    CTA = Colegios Total Aprobados
+    CHA = Colegios Hombres Aprobados
+    CMA = Colegios Mujeres Aprobadas
+    CPU = Colegio Publica
+    CPR = Colegio Privada
+    CSV = Colegio Subvencionada
+    CUR = Colegio Urbana
+    CRU = Colegio Rural 
+    CTAA = Colegio Tasa de Aprobacion
+    CTAAH = Colegio Tasa de Aprobacion Hombres
+    CTAAM = Colegio Tasa de Aprobacion Mujeres 
+    CEXM = Colegio Extranjeros Matriculads
+    CPEXM = Colegio Proporcion Extranjeros Matriculads
+    CT = Colegios en Total
+    CPPR = Colegios Proporcion de Privados
+    CPRU = Colegios Proporcion de Rurales
+    
+    TCP = Tasa Crecimiento de Población (2011 - 2016)  
+"""
 
-#TOTT: Total Extranjeros
-#TOTH: Total Hombres Extranjeros
-#TOTM: Total Mujeres Extranjeros
-##############################################
+
 
 
 def group_escuelas(escuelas_df):
@@ -22,60 +74,64 @@ def group_escuelas(escuelas_df):
     escuelas_df = pd.concat([escuelas_df, pd.get_dummies(escuelas_df['SECTOR'], prefix='SECTOR')], axis=1)
     escuelas_df = pd.concat([escuelas_df, pd.get_dummies(escuelas_df['ZONA'], prefix='ZONA')], axis=1)
     group = escuelas_df.groupby(['ZIPCODE'])
-    df = group.agg(ESCUELAS_MATRICULADOS=('MFT', 'sum'),
-                   ESCUELAS_HOMBRES_MATRICULADOS=('MFH', 'sum'),
-                   ESCUELAS_MUJERES_MATRICULADAS=('MFM', 'sum'),
-                   ESCUELAS_REPROBADOS=('RET', 'sum'),
-                   ESCUELAS_HOMBRES_REPROBADOS=('REH', 'sum'),
-                   ESCUELAS_MUJERES_REPROBADAS=('REM', 'sum'),
-                   ESCUELAS_APROBADOS=('APT', 'sum'),
-                   ESCUELAS_HOMBRES_APROBADOS=('APH', 'sum'),
-                   ESCUELAS_MUJERES_APROBADAS=('APM', 'sum'),
-                   ESCUELAS_PUBLICAS=('SECTOR_1', 'sum'),
-                   ESCUELAS_PRIVADAS=('SECTOR_2', 'sum'),
-                   ESCUELAS_SUBVENCIONADAS=('SECTOR_3', 'sum'),
-                   ESCUELAS_RURALES=('ZONA_1', 'sum'),
-                   ESCUELAS_URBANAS=('ZONA_2', 'sum'))
+    df = group.agg(ETM=('MFT', 'sum'),
+                   EHM=('MFH', 'sum'),
+                   EMM=('MFM', 'sum'),
+                   ETR=('RET', 'sum'),
+                   EHR=('REH', 'sum'),
+                   EMR=('REM', 'sum'),
+                   ETA=('APT', 'sum'),
+                   EHA=('APH', 'sum'),
+                   EMA=('APM', 'sum'),
+                   EPU=('SECTOR_1', 'sum'),
+                   EPR=('SECTOR_2', 'sum'),
+                   ESV=('SECTOR_3', 'sum'),
+                   EUR=('ZONA_1', 'sum'),
+                   ERU=('ZONA_2', 'sum'))
 
-    df['ESCUELAS_TAZA_APROBACION'] = df['ESCUELAS_APROBADOS'] / df['ESCUELAS_MATRICULADOS']
-    df['ESCUELAS_TAZA_APROBACION_HOMBRES'] = df['ESCUELAS_HOMBRES_APROBADOS'] / df['ESCUELAS_HOMBRES_MATRICULADOS']
-    df['ESCUELAS_TAZA_APROBACION_MUJERES'] = df['ESCUELAS_MUJERES_APROBADAS'] / df['ESCUELAS_MUJERES_MATRICULADAS']
+    df['ETAA'] = df['ETA'] / df['ETM']
+    df['ETAAH'] = df['EHA'] / df['EHM']
+    df['ETAAM'] = df['EMA'] / df['EMM']
     return df
+
 
 def group_colegios(colegios_df):
     # expand the categorical information
     colegios_df = pd.concat([colegios_df, pd.get_dummies(colegios_df['SECTOR'], prefix='SECTOR')], axis=1)
     colegios_df = pd.concat([colegios_df, pd.get_dummies(colegios_df['ZONA'], prefix='ZONA')], axis=1)
     group = colegios_df.groupby(['ZIPCODE'])
-    df = group.agg(COLEGIOS_MATRICULADOS=('MFT', 'sum'),
-                   COLEGIOS_HOMBRES_MATRICULADOS=('MFH', 'sum'),
-                   COLEGIOS_MUJERES_MATRICULADAS=('MFM', 'sum'),
-                   COLEGIOS_REPROBADOS=('RET', 'sum'),
-                   COLEGIOS_HOMBRES_REPROBADOS=('REH', 'sum'),
-                   COLEGIOS_MUJERES_REPROBADAS=('REM', 'sum'),
-                   COLEGIOS_APROBADOS=('APT', 'sum'),
-                   COLEGIOS_HOMBRES_APROBADOS=('APH', 'sum'),
-                   COLEGIOS_MUJERES_APROBADAS=('APM', 'sum'),
-                   COLEGIOS_PUBLICOS=('SECTOR_1', 'sum'),
-                   COLEGIOS_PRIVADOS=('SECTOR_2', 'sum'),
-                   COLEGIOS_SUBVENCIONADOS=('SECTOR_3', 'sum'),
-                   COLEGIOS_RURALES=('ZONA_1', 'sum'),
-                   COLEGIOS_URBANOS=('ZONA_2', 'sum'))
+    df = group.agg(CTM=('MFT', 'sum'),
+                   CHM=('MFH', 'sum'),
+                   CMM=('MFM', 'sum'),
+                   CTR=('RET', 'sum'),
+                   CHR=('REH', 'sum'),
+                   CMR=('REM', 'sum'),
+                   CTA=('APT', 'sum'),
+                   CHA=('APH', 'sum'),
+                   CMA=('APM', 'sum'),
+                   CPU=('SECTOR_1', 'sum'),
+                   CPR=('SECTOR_2', 'sum'),
+                   CSV=('SECTOR_3', 'sum'),
+                   CUR=('ZONA_1', 'sum'),
+                   CRU=('ZONA_2', 'sum'))
 
-    df['COLEGIOS_TAZA_APROBACION'] = df['COLEGIOS_APROBADOS'] / df['COLEGIOS_MATRICULADOS']
-    df['COLEGIOS_TAZA_APROBACION_HOMBRES'] = df['COLEGIOS_HOMBRES_APROBADOS'] / df['COLEGIOS_HOMBRES_MATRICULADOS']
-    df['COLEGIOS_TAZA_APROBACION_MUJERES'] = df['COLEGIOS_MUJERES_APROBADAS'] / df['COLEGIOS_MUJERES_MATRICULADAS']
+    df['CTAA'] = df['CTA'] / df['CTM']
+    df['CTAAH'] = df['CHA'] / df['CHM']
+    df['CTAAM'] = df['CMA'] / df['CMM']
     return df
+
 
 def group_extranjeros_escuelas(escuelas_df):
     group = escuelas_df.groupby(['ZIPCODE'])
-    df = group.agg(ESCUELAS_EXTRANJEROS_MATRICULADOS=('TOTT', 'sum'))
+    df = group.agg(EEXM=('TOTT', 'sum'))
     return df
+
 
 def group_extranjeros_colegios(colegios_df):
     group = colegios_df.groupby(['ZIPCODE'])
-    df = group.agg(COLEGIOS_EXTRANJEROS_MATRICULADOS=('TOTT', 'sum'))
+    df = group.agg(CEXM=('TOTT', 'sum'))
     return df
+
 
 def group_crimenes(crimenes_df):
     crimenes_df = pd.concat([crimenes_df, pd.get_dummies(crimenes_df['CATEGORY'])], axis=1)
@@ -98,10 +154,8 @@ extranjeros_colegios_df = pd.read_csv('data/extranjeros_colegios.csv')
 extranjeros_escuelas_df = pd.read_csv('data/extranjeros_escuelas.csv')
 crimenes_df = pd.read_csv('data/crimenes.csv')
 
-
-
 grouped_escuelas = group_escuelas(escuelas_df)
-grouped_colegios = group_colegios(escuelas_df)
+grouped_colegios = group_colegios(colegios_df)
 grouped_estranjeros_escuelas = group_extranjeros_escuelas(extranjeros_escuelas_df)
 grouped_estranjeros_colegios = group_extranjeros_colegios(extranjeros_escuelas_df)
 grouped_crimenes_df = group_crimenes(crimenes_df)
@@ -112,26 +166,26 @@ df = pd.merge(df, grouped_estranjeros_escuelas, on='ZIPCODE')
 df = pd.merge(df, grouped_estranjeros_colegios, on='ZIPCODE')
 df = pd.merge(df, grouped_crimenes_df, on='ZIPCODE')
 
-df['TAZA_CRECIMIENTO_POBLACION'] = (df['POBLACION_2016'] - df['POBLACION_2011'])/df['POBLACION_2016']
+df['TCP'] = (df['POBLACION_2016'] - df['POBLACION_2011'])/df['POBLACION_2016']
 
-df['TAZA_EXTRANJEROS_ESCUELAS'] = df['ESCUELAS_EXTRANJEROS_MATRICULADOS'] / df['ESCUELAS_MATRICULADOS']
-df['TAZA_EXTRANJEROS_COLEGIOS'] = df['COLEGIOS_EXTRANJEROS_MATRICULADOS'] / df['COLEGIOS_MATRICULADOS']
+df['EPEXM'] = df['EEXM'] / df['ETM']
+df['CPEXM'] = df['CEXM'] / df['CTM']
 
-df['TOTAL_ESCUELAS'] = df['ESCUELAS_PUBLICAS'] + df['ESCUELAS_PRIVADAS'] + df['ESCUELAS_SUBVENCIONADAS']
-df['PROPORCION_ESCUELAS_PRIVADAS'] = df['ESCUELAS_PRIVADAS'] / df['TOTAL_ESCUELAS']
-df['PROPORCION_ESCUELAS_RURALES'] = df['ESCUELAS_RURALES'] / df['TOTAL_ESCUELAS']
+df['ET'] = df['EPU'] + df['EPR'] + df['ESV']
+df['EPPR'] = df['EPR'] / df['ET']
+df['EPRU'] = df['ERU'] / df['ET']
 
-df['TOTAL_COLEGIOS'] = df['COLEGIOS_PUBLICOS'] + df['COLEGIOS_PRIVADOS'] + df['COLEGIOS_SUBVENCIONADOS']
-df['PROPORCION_COLEGIOS_PRIVADOS'] = df['COLEGIOS_PRIVADOS'] / df['TOTAL_COLEGIOS']
-df['PROPORCION_COLEGIOS_RURALES'] = df['COLEGIOS_RURALES'] / df['TOTAL_COLEGIOS']
+df['CT'] = df['CPU'] + df['CPR'] + df['CSV']
+df['CPPR'] = df['CPR'] / df['CT']
+df['CPRU'] = df['CRU'] / df['CT']
 
-df['TAZA_ASALTO'] = df['ASALTO'] / df['POBLACION_2016']
-df['TAZA_HOMICIDIO'] = df['HOMICIDIO'] / df['POBLACION_2016']
-df['TAZA_HURTO'] = df['HURTO'] / df['POBLACION_2016']
-df['TAZA_ROBO'] = df['ROBO'] / df['POBLACION_2016']
-df['TAZA_ROBO_VEHICULO'] = df['ROBO_VEHICULO'] / df['POBLACION_2016']
-df['TAZA_TACHA_VEHICULO'] = df['TACHA_VEHICULO'] / df['POBLACION_2016']
+df['TASA_ASALTO'] = df['ASALTO'] / df['POBLACION_2016']
+df['TASA_HOMICIDIO'] = df['HOMICIDIO'] / df['POBLACION_2016']
+df['TASA_HURTO'] = df['HURTO'] / df['POBLACION_2016']
+df['TASA_ROBO'] = df['ROBO'] / df['POBLACION_2016']
+df['TASA_ROBO_VEHICULO'] = df['ROBO_VEHICULO'] / df['POBLACION_2016']
+df['TASA_TACHA_VEHICULO'] = df['TACHA_VEHICULO'] / df['POBLACION_2016']
 
 
-df.to_csv('data/processed/processed.csv', encoding='utf-8-sig')
+df.to_csv('data/processed/processed.csv', encoding='utf-8-sig', index=False)
 
